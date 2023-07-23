9031f72033d856395cf4eeda8ec76e4cc34d8806
Ashraf Azemi <ashraf.chzm@gmail.com>
2023-07-23 22:58:23 +0800

    category add

--- src/component/UserDetail/User-Detail-Container.jsx
+++ src/component/UserDetail/User-Detail-Container.jsx
@@ -1,30 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import "./User-Detail-Container.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEllipsisVertical,
+  faPlus,
+  faTrashCan,
+  faMinus,
+} from "@fortawesome/free-solid-svg-icons";
 
 const UserDetailContainer = () => {
+  const [categoryList, setCategoryList] = useState([]);
+  const [addCategory, setAddCategory] = useState(false);
+  const [singleCategory, setSingleCategory] = useState({});
+
+  const onHandleChange = (e) => {
+    setSingleCategory({ id: 1, name: e.target.value });
+    console.log("single", singleCategory);
+  };
+
+  const openInputModal = () => {
+    setAddCategory(true);
+  };
+  const closeInputModal = () => {
+    setAddCategory(false);
+  };
+
+  const AddCategory = (e) => {
+    e.preventDefault();
+    setCategoryList([...categoryList, singleCategory]);
+    setAddCategory(false);
+  };
+
   return (
     <div className="user-detail-container">
       <div className="user-status-container">
-        <span>logo</span>
         <p>Ashraf</p>
-        <span>logo</span>
+        <FontAwesomeIcon icon={faEllipsisVertical} />
       </div>
       <div className="category-container">
         <div className="head-container">
           <h4>CATEGORIES</h4>
-          <p>+</p>
+          {!addCategory ? (
+            <FontAwesomeIcon onClick={openInputModal} icon={faPlus} />
+          ) : (
+            <FontAwesomeIcon onClick={closeInputModal} icon={faMinus} />
+          )}
         </div>
-        <div>category type</div>
-        <div>category type</div>
-        <div>category type</div>
-        <div>category type</div>
-        <div>category type</div>
-        <div>category type</div>
-        <div>category type</div>
-        <div>category type</div>
+        {addCategory && (
+          <form onSubmit={(e) => AddCategory(e)} className="add-category-form">
+            <input type="text" onChange={(e) => onHandleChange(e)} />
+          </form>
+        )}
+        {categoryList.map((e, index) => {
+          return <div key={index}>{e.name}</div>;
+        })}
       </div>
       <div className="reset-container">
-        <span>logo</span>
+        <FontAwesomeIcon icon={faTrashCan} />
         <p style={{ marginLeft: "10px" }}>Reset</p>
       </div>
     </div>
