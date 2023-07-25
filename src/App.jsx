d4e7cb6fd71a7ed5f6682ddc2534aa49262b606a
Ashraf Azemi <ashraf.chzm@gmail.com>
2023-07-25 23:52:47 +0800

    connecting server

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import UserDetailContainer from "./component/UserDetail/User-Detail-Container";
 import AddNoteContainer from "./component/AddNote/Add-Note-Container";
 import NoteFormContainer from "./component/NoteForm/Note-Form-Container";
@@ -6,8 +7,25 @@ import NoteFormContainer from "./component/NoteForm/Note-Form-Container";
 import "./App.css";
 
 function App() {
+  const [home, setHome] = useState("");
+
+  const getDatas = async () => {
+    const response = await axios.get("http://localhost:3001/getData");
+    setHome(response.data);
+  };
+
+  useEffect(() => {
+    // fetch("/api")
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     setHome(data.json());
+    //     console.log(data);
+    //   });
+    getDatas();
+  }, []);
   return (
     <div className="main-container">
+      <p>{home}</p>
       <UserDetailContainer />
       <AddNoteContainer />
       <NoteFormContainer />
